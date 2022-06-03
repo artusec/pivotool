102c0b2dfce91098fb65afcc4b82682a0bb51a34
Arturo Aguirre Calvo <arturo.aguirrecalvo@telefonica.com>
2022-06-03 12:22:21 +0200

    Rebranding

--- pivotool.sh
+++ pivotool.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ################################################################################
-# pivotool -- by artuyero                                                      #
+# pivotool -- by artusec                                                       #
 #                                                                              #
 # pivotool in ethical hacking, as expected, is a simple tool written in bash   #
 # that could help you in the post exploitation phase to pivot to other systems.#
